7dfb9077953ead006fa6a3be564c67f9662a5853
Stephatt <ttt1089@126.com>
2013-12-05 20:20:19 +0800

    写函数，输出前n个素数
    改正了判断余数部分；缩小了取数范围。

--- main.c
+++ main.c
@@ -8,39 +8,50 @@
 
 #include <stdio.h>
 
+
 int main()
 {
 
-    void print_prime(int m);
+    void print_prime(int);
     int n;
     printf("输入一个数:n=");
-    scanf("n=%d",&n);
-    void print_prime(int n);
-
+    scanf("%d",&n);
+    print_prime(n);
+   
     
 }
 
 
 void print_prime(int m)
 {
-    int a[500],i,j,h,k=0,p[100]={0};
-    for (i=0; i<500; i++) {
-        a[i]=i+1;
+    int a[500]={1},i,j=0;
+    for (i=1; i<500; i+=2) {
+        a[i]=i+2;           //为了简便计算，可以只录入奇数
     }
-    for (i=0; i<500; i++) {
-        for (j=0; j<500; j++) {
-            p[k]=a[i];
-            for (h=k; h<500; h++) {
-                if (a[h]%p[k]==0) {
-                    a[h]=0;
-                }
+    for (i=2; i<500; i++) {
+        for (j=i+1; j<=500; j++) {
+            if (a[j]&&a[j]%i==0) {  //因为之前的计算可能已经将数组中的一部分数归零了，所以要去除为零的值
+                    a[j]=0;
             }
-            k++;
         }
     }
-    printf("输出前n个素数:");
-    for (i=0; i<m; i++) {
-        printf("prime=%d",a[i]);
+    for (i=0; i<500; i++) {
+        printf("%d",a[i]);
     }
-    return;
+    
+    printf("输出前n个素数:");
+    printf("\n");
+    int k=0;
+    for (i=0; i<m; i++) {
+        for (;k<500;) {
+            if (a[k]==0) {
+                k++;continue;}
+            else {
+                printf("prime=%d",a[k]);
+                printf("\n");
+                k++;}
+            
+        break;
+        }
+    }
 }
